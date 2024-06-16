3ae5094f559ddee364e8c1c23b2935a9acfa9a49
paranoidbarbie <brown.hannah2024@gmail.com>
2024-06-16 12:17:00 +0100

    First Commit

---
+++ colour.py
@@ -0,0 +1,51 @@
+from colorthief import ColorThief
+import os
+
+def importColour(wallpaper = "/home/yoru/Pictures/wallpaper/sunsetwindow.png"):
+    ct = ColorThief("{}".format(wallpaper))
+    colours = ct.get_palette(color_count=18)
+    palette = []
+    for colour in colours: 
+        colour = palette.append(f"#{colour[0]:02x}{colour[1]:02x}{colour[2]:02x}")
+    '''
+    background = palette[0]
+    
+    foreground = palette[1]
+    colour1 = palette[2]
+    colour2 = palette[3]
+    '''
+    changeColour(palette)
+
+
+def changeColour(palette):
+    path = f"/home/{os.getlogin()}/.config/kitty/themes/"
+    filename = os.path.join(path, 'night.conf')
+    colourSteps = 16
+    colourDict = {}
+
+    colourOne = palette[0]
+    colourTwo = palette[1]
+
+    basicDict = {
+        'background' : colourOne,
+        'foreground' : colourTwo
+            }
+    
+
+    for i in range(2,colourSteps):
+        key1 = f'color{i-2}'
+        key2 = palette[i]
+        colourDict[key1] = key2
+    
+    with open(filename, 'w') as file:
+        for key, value in basicDict.items():
+            file.write(f'{key} {value}\n')
+
+        for colour, palette in colourDict.items():
+            file.write(f'{colour} {palette}\n')
+    os.system("kill -s USR1 $(pidof kitty)") 
+
+if __name__ == "__main__":
+    importColour()
+
+
---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import time
+from colour import importColour
+
+directory = f'/home/{os.getlogin()}/Pictures/wallpaper/'
+os.chdir(directory)
+
+def changeImage():
+    image_format = ["png", "jpg"]
+    images = [image for image in os.listdir() if image.endswith(tuple(image_format))]
+    for image_name in images:
+        wallpaper = os.path.join(directory, image_name)
+        setImage(wallpaper)
+
+
+        
+def setImage(wallpaper):
+    os.system(f'hyprctl hyprpaper preload "{wallpaper}"')
+    time.sleep(3)
+    os.system(f'hyprctl hyprpaper wallpaper "eDP-1,{wallpaper}"')
+    os.system('hyprctl dispatch exec hyprpaper')
+    importColour(wallpaper)
+    
+
+def takeEffect():
+    while True:
+        time.sleep(3)
+        changeImage()
+
+if __name__ == "__main__":
+    takeEffect()
